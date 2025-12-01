41ed3fa6d1730a70cc1a0e86242c2c9029e1e7fe
Tharcio Santos <tharcio09@hotmail.com>
2025-12-01 04:10:18 -0300

    chore: Setup inicial

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Setup Inicial Concluído 🚀
+      </h1>
+    </div>
+  )
+}
